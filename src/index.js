46751c473292421512775e7cf79eaba6c78508c6
abadanaf <abadanafchala@gmail.com>
2024-12-03 17:41:46 +0300

    the tutor updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hey There!</h1>
+
+
+const myElement = (
+  <>
+<h1>React Practice</h1>
+  <table>
+  <h1>My Profile</h1>
+    <tr>
+      <th>My Name</th>
+    </tr>
+    <tr>
+      <td>Abadan</td>
+    </tr>
+    <tr>
+      <td>chala</td>
+    </tr>
+  </table>
+  <h1>My favourite Languages</h1>
+  <ul>
+    <li>HTML</li>
+    <li>BOOTSTRAP</li>
+    <li>REACT</li>
+  </ul>
+  </>
+  
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+
+root.render(myElement);
